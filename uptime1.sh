2c896bf3d4bb9736708ba45b31830103b0abf8bd
Deepak <deepakdev60513@gmail.com>
2023-03-28 08:51:38 +0000

    V1

--- uptime1.sh
+++ uptime1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 a=`uptime`
-echo "Hello DeepakRH: my sys is up since $a"
+echo "Hello Deepak: my sys is up since $a"
 echo "Line updated with GUI"
 echo "1line added - Updated"
 echo " 1 line "
